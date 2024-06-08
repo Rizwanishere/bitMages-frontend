ca9053c1e40dacf9ac30339aa1aa9cdbe1d4d166
Rizwanishere <rizwantopper@gmail.com>
2024-06-08 12:50:46 +0530

    Added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary' : '#0e7490',
+        // 'secondary' : '#59A3F9',
+        // 'tertiary' : '#707070'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '::selection': {
+          backgroundColor: '#0e7490',
+          color: '#ffffff',
+        },
+        '::-moz-selection': {
+          backgroundColor: '#0e7490',
+          color: '#ffffff',
+        },
+      });
+    },
+  ],
 }
-
